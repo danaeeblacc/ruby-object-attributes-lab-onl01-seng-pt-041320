181f62a2f1c75b87d56b34f65ba1d812fd340ac3
Teinelle Charles <teinelledcharles@gmail.com>
2020-05-17 06:11:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,17 @@
 Class Dog
-  def name=(dog)
+  def name=(Dog)
     @name = dog
   end
+
+  def name
+    @name
+  end
+
+  def breed=(breed)
+    @breed= breed
+  end
+
+  def breed
+    @breed
+  end
+end
